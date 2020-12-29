55c8e0b6499063359fa7dc15519f9d655b8c8dcb
Holasek, Vit <vit.holasek@siemens.com>
2020-12-29 19:05:21 +0100

    Fixed incompatibilities with new ESP-IDF

--- main/main.c
+++ main/main.c
@@ -71,13 +71,14 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
     {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "Got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
 
 void wifi_init()
 {
+    esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
@@ -171,8 +172,8 @@ void app_main(void)
 {
 	wifi_event_group = xEventGroupCreate();
     nvs_init();
-    tcpip_adapter_init();
-    esp_event_loop_create_default();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // Start Wi-Fi connection
     ESP_LOGI(TAG, "WiFi init");
